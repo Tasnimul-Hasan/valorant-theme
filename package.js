ea237cfe2f46b5572a7a6bd6a114f181d865b685
Tasnimul Hasan <589.dreamer.589@gmail.com>
2022-07-15 21:11:42 +0600

    4.1.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'valorant-theme',
-  version: '4.1.1',
+  version: '4.1.2',
   displayName: 'Valorant Theme',
   description: 'This theme is based on the FPS game called valorant!',
   publisher: 'TasnimulHasan',
